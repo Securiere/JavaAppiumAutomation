7ad1c91256dfb602be6c6d2431a9703e85c30649
Securiere <93787522+Securiere@users.noreply.github.com>
2021-11-07 17:08:45 +0300

    1

---
+++ src/MainClass.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainClass
+{
+    @Test
+    public void getLocalNumber()
+    {
+        int number = 14;
+        System.out.println(number);
+    }
+}
+
